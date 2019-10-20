b2db546a4d584285c67951ad40db99c77cfca36e
Marc Nützel <marc.nuetzel@me.com>
2019-10-20 18:12:41 +0200

    initial

---
+++ spock-junit-comparison/src/main/java/de/marcnuetzel/spockjunitcomparison/SpockJunitComparisonApplication.java
@@ -0,0 +1,13 @@
+package de.marcnuetzel.spockjunitcomparison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpockJunitComparisonApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpockJunitComparisonApplication.class, args);
+  }
+
+}
